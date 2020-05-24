9f8ff1281c1480cd835fface7d19a4dd04ebcbd2
unknown <madis.tepp@gmail.com>
2020-05-24 19:34:01 +0200

    logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 # Create your views here.
 
 def index(request):
     """Return the index html file"""
     return render(request, 'index.html')
+
+
+def logout(request):
+    """Logout the user"""
+    auth.logout(request)
+    messages.success(request, 'You have been logged out Yo!')
+    return redirect(reverse('index'))
+
--- django_auth/urls.py
+++ django_auth/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from accounts.views import index 
+from accounts.views import index, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name='index'),
+    path('accounts/logout/$',logout, name='logout')
 ]
